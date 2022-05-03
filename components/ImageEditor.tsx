33058626c525393066fa47a53082ba3952ca5f45
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-05-03 16:53:46 +0900

    feat: improve text2, text3 hider

--- components/ImageEditor.tsx
+++ components/ImageEditor.tsx
@@ -25,13 +25,38 @@ const text1HideDivStyle: CSSProperties = {
   alignItems: "center",
   transform: "rotate(-5deg)",
   backgroundColor: "#fff",
+  zIndex: "999",
+};
+
+const text2HideDivStyle: CSSProperties = {
+  position: "absolute",
+  top: "28%",
+  left: "-5%",
+  width: "50%",
+  height: "7%",
+  transform: "rotate(1deg)",
+  clipPath: "polygon(0 0, 97% 0, 93% 100%, 0% 100%)",
+  backgroundColor: "#191F2E",
+};
+
+const text3HideDivStyle: CSSProperties = {
+  position: "absolute",
+  top: "29%",
+  left: "60%",
+  width: "50%",
+  height: "8%",
+  transform: "rotate(1deg)",
+  clipPath: "polygon(6% 0, 100% 0, 100% 100%, 10.5% 100%)",
+  backgroundColor: "#191F2E",
 };
 
 const ImageEditor: FC<ImageEditorProps> = ({ text1 }): JSX.Element => {
   return (
-    <div className="relative">
+    <div className="relative overflow-hidden">
       <div style={bannerHideDivStyle} />
       <div style={text1HideDivStyle}>{text1}</div>
+      <div style={text2HideDivStyle} />
+      <div style={text3HideDivStyle} />
       <img src="/not_true.png" alt="not_true" className="w-full" />
     </div>
   );
