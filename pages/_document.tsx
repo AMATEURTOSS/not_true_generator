3a40ba8a105b76e459ebb2d605c1a79d4b81bd2c
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-05-04 11:13:50 +0900

    feat: add meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,14 @@ export default function Document() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="favicon.png" type="image/png" />
+        <meta name="description" content="사실이 아닙니다! 생성기" />
+        <meta property="og:title" content="사실이 아닙니다! 생성기" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="favicon.png" />
+        <meta property="og:description" content="사실이 아닙니다! 생성기" />
+        <meta name="twitter:image" content="favicon.png" />
+        <meta name="twitter:description" content="사실이 아닙니다! 생성기" />
+        <meta name="theme-color" content="#6495ED" />
       </Head>
       <body>
         <Main />
