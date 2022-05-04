8e57e86530cdcd60b0eb9b6c0eb44ea08447bde7
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-05-04 11:06:37 +0900

    feat: update html lang to ko

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ko">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="favicon.png" type="image/png" />
