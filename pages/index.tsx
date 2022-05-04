6f9140fced31a6ee4e97e5c20a4fcec51655baee
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-05-04 11:01:15 +0900

    feat: add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useState } from "react";
 import domtoimage from "dom-to-image";
 import ImageEditor from "../components/ImageEditor";
@@ -33,19 +34,27 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="m-auto w-full max-w-xl p-6">
-      <ImageEditor text1={text1} isTrue={isTrue} />
-      <TextInput label="text1" text={text1} setText={setText1} />
-      <SelectTwoOfOneButton
-        button1InnerText="사실이 아닙니다!"
-        button2InnerText="사실이 맞습니다!"
-        isTrue={isTrue}
-        setIsTrue={setIsTrue}
-      />
-      <button className="mt-8 w-full bg-blue-400 p-3 text-white" onClick={save}>
-        저장하기
-      </button>
-    </div>
+    <>
+      <Head>
+        <title>사실이 아닙니다! 생성기</title>
+      </Head>
+      <div className="m-auto w-full max-w-xl p-6">
+        <ImageEditor text1={text1} isTrue={isTrue} />
+        <TextInput label="text1" text={text1} setText={setText1} />
+        <SelectTwoOfOneButton
+          button1InnerText="사실이 아닙니다!"
+          button2InnerText="사실이 맞습니다!"
+          isTrue={isTrue}
+          setIsTrue={setIsTrue}
+        />
+        <button
+          className="mt-8 w-full bg-blue-400 p-3 text-white"
+          onClick={save}
+        >
+          저장하기
+        </button>
+      </div>
+    </>
   );
 };
 
