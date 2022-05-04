c505081256efaaf9f03443b531be0e0b9e24393e
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-05-04 10:31:55 +0900

    feat: add isTrue control button

---
+++ components/SelectTwoOfOneButton.tsx
@@ -0,0 +1,46 @@
+import { Dispatch, FC, SetStateAction, useEffect, useRef } from "react";
+
+interface SelectTwoOfOneButtonProps {
+  button1InnerText: string;
+  button2InnerText: string;
+  isTrue: boolean;
+  setIsTrue: Dispatch<SetStateAction<boolean>>;
+}
+
+const SelectTwoOfOneButton: FC<SelectTwoOfOneButtonProps> = ({
+  button1InnerText,
+  button2InnerText,
+  isTrue,
+  setIsTrue,
+}): JSX.Element => {
+  const button1 = useRef<HTMLButtonElement>(null);
+  const button2 = useRef<HTMLButtonElement>(null);
+
+  return (
+    <div className="mt-6">
+      <span className="block text-gray-400">사실 여부</span>
+      <div className="flex items-center">
+        <button
+          className={`w-1/2 border p-3 ${
+            isTrue ? "" : "bg-blue-400 text-white"
+          }`}
+          ref={button1}
+          onClick={() => setIsTrue(false)}
+        >
+          {button1InnerText}
+        </button>
+        <button
+          className={`w-1/2 border p-3 ${
+            isTrue ? "bg-blue-400 text-white" : ""
+          }`}
+          ref={button2}
+          onClick={() => setIsTrue(true)}
+        >
+          {button2InnerText}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SelectTwoOfOneButton;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import ImageEditor from "../components/ImageEditor";
 import { useState } from "react";
 import TextInput from "../components/TextInput";
+import SelectTwoOfOneButton from "../components/SelectTwoOfOneButton";
 
 const Home: NextPage = () => {
   const [text1, setText1] = useState<string>(
@@ -12,6 +13,12 @@ const Home: NextPage = () => {
   return (
     <div className="m-auto w-full max-w-xl p-6">
       <ImageEditor text1={text1} isTrue={isTrue} />
+      <SelectTwoOfOneButton
+        button1InnerText="사실이 아닙니다!"
+        button2InnerText="사실이 맞습니다!"
+        isTrue={isTrue}
+        setIsTrue={setIsTrue}
+      />
       <TextInput label="text1" text={text1} setText={setText1} />
     </div>
   );
