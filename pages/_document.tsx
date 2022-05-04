54fb1d18fb5e484e2a3b3eedb2d38ab9177d35f9
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-05-04 11:04:48 +0900

    feat: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="favicon.png" type="image/png" />
       </Head>
       <body>
         <Main />
